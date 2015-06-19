ceb951aeb619dc4cb3397c12a28824c163207f0f
Hack Reactor Students <accounts@hackreactor.com>
2015-06-18 19:02:51 -0700

    Add geolocation service which gets current location Close #6

--- www/js/home/home.js
+++ www/js/home/home.js
@@ -2,4 +2,9 @@ angular.module('app.home', [])
 
   .controller('HomeController', function($scope, LocationService) {
 
+    //on load get the user's current location
+    LocationService.getCurrentLocation(function(currentLocation) {
+      $scope.loc = currentLocation;
+    });
+
   })
--- www/js/services/services.js
+++ www/js/services/services.js
@@ -1,5 +1,26 @@
-angular.module('app.services', [])
+angular.module('app.services', [
+  'ngCordova'
+  ])
 
-  .service('LocationService', function() {
-
-  })
+  .service('LocationService', function($cordovaGeolocation, $ionicPlatform) {
+    // takes a callback whose first argument contains current location
+    this.getCurrentLocation = function(callback) {
+      var options = {
+        timeout: 10000,
+        enableHighAccuracy: false
+      };
+      // Note: cordova plugins must be wrapped in document.ready or $ionicPlatforml.ready
+      $ionicPlatform.ready(function(){
+        $cordovaGeolocation.getCurrentPosition(options)
+          .then(function (position) {
+            callback({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude
+            });
+          }, function(err) {
+            //handle error
+            console.log(err)
+          });
+      });
+    }
+  });
