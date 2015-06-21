724de5e4585404229b9a17704738c980aaabb31f
Sat Mandir Khalsa <smkhalsa@gmail.com>
2015-06-20 17:54:14 -0700

    Use deployed restbus server

--- www/js/services/services.js
+++ www/js/services/services.js
@@ -28,15 +28,17 @@ angular.module('app.services', [
   .service('RestBusService', function($http) {
     this.getRoute = function() {
       return $http({
-        url: 'http://localhost:3535/agencies/sf-muni/routes',
+        url: 'http://mybus-server.herokuapp.com/agencies/sf-muni/routes',
+        // url: 'http://localhost:3000/agencies/sf-muni/routes',
         method: 'GET'
-      })
+      });
     };
 
     this.getStops = function(latlon) {
       return $http({
-        url: 'http://localhost:3535/locations/' + latlon.latitude + ',' + latlon.longitude + '/predictions',
+        url: 'http://mybus-server.herokuapp.com/locations/' + latlon.latitude + ',' + latlon.longitude + '/predictions',
+        // url: 'http://localhost:3000/locations/' + latlon.latitude + ',' + latlon.longitude + '/predictions',
         method: 'GET'
-      })
+      });
     };
   });
