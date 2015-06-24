ea5333bf893f50ea66dfea8cb13b44754cf370cd
Michael Borglin <Borglin.Me@gmail.com>
2015-06-24 16:23:23 -0700

    Another try at protractor in travis

--- www/spec/unit/myBusSpec.js
+++ www/spec/unit/myBusSpec.js
@@ -2,9 +2,9 @@ describe("myBus", function(){
 
   it("should get the user's location", function(){
     expect(true).toEqual(true);
-  })
+  });
 
-})
+});
 
 describe("HomeController", function() {
 
