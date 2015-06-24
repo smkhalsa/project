6cc0952f6ec43a55f28fc67c37bf1362d1ef3ad1
Michael Borglin <Borglin.Me@gmail.com>
2015-06-24 15:26:05 -0700

    Bufix in karma configuration

--- www/spec/karma.conf.js
+++ www/spec/karma.conf.js
@@ -2,7 +2,7 @@
 // Generated on Tue Jun 23 2015 10:51:19 GMT-0700 (PDT)
 
 module.exports = function(config) {
-  config.set({
+  var configuration = {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
@@ -64,9 +64,11 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  });
+  };
 
   if (process.env.TRAVIS) {
     configuration.browsers = ['Chrome_travis_ci'];
   }
+
+  config.set(configuration);
 };
