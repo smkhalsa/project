9be7e9d5341cb5e50dc7f6297d4735ac37b604fa
Terry <tchan247@gmail.com>
2015-06-27 15:31:58 -0700

    Refactor controller code into services; adjust map display;

--- www/js/busRoutes/details.js
+++ www/js/busRoutes/details.js
@@ -1,66 +1,17 @@
 angular.module('app.details', [])
 
-    //.controller('DetailsController', function($scope, $ionicHistory, $location, LocationService, RestBusService, ReadFileService, $stateParams) {
-    .controller('DetailsController', function($scope, route) {
-      $scope.route = route;
-    //
-    //var init = function() {
-    //  $scope.routeDetails = PageChangeService.currentRoute;
-    //  $scope.getLocation(function(currentLocation) {
-    //    $scope.loc = currentLocation;
-    //    // load map
-    //    // var sanFran = {lat: 37.78, lng: -122.416}
-    //    $scope.mapOptions = {center: {lat: $scope.loc.latitude, lng: $scope.loc.longitude}, zoom: 17};
-    //    $scope.map = new google.maps.Map(document.getElementById('mapContainer'), $scope.mapOptions);
-    //
-    //    $scope.getStops()
-    //    .then(function() {
-    //      $scope.getStopLocation()
-    //      .then(function() {
-    //        $scope.renderMapMarkers();
-    //      });
-    //    })
-    //  });
-    //};
-    //
-    //$scope.getLocation = function(callback) {
-    //  LocationService.getCurrentLocation(callback);
-    //};
-    //
-    //$scope.getStops = function() {
-    //   return RestBusService.getStops($scope.loc)
-    //  .then(function() {
-    //    $scope.routeInfo = PageChangeService.currentRoute;
-    //    $scope.stations = {};
-    //  });
-    //}
-    //
-    //$scope.getStopLocation = function() {
-    //  // get list of stations
-    //  return ReadFileService.readFile('../stops.json')
-    //  .then(function(data) {
-    //    $scope.stations = data.data;
-    //  });
-    //};
-    //
-    //$scope.renderMapMarkers = function() {
-    //  // mark user location
-    //  var userMarker = new google.maps.Marker({
-    //    position: new google.maps.LatLng($scope.loc.latitude, $scope.loc.longitude),
-    //    map: $scope.map,
-    //    title: 'user',
-    //    icon: './img/user.png'
-    //  });
-    //
-    //  var currentStation = $scope.stations[$scope.routeDetails.stop.id];
-    //  // mark station
-    //  var stationMarker = new google.maps.Marker({
-    //    position: new google.maps.LatLng(currentStation.lat, currentStation.lon),
-    //    map: $scope.map,
-    //    title: 'station'
-    //  });
-    //};
-    //
-    //init();
+  .controller('DetailsController', function($scope, route, LocationService, RestBusService, MapService, VehiclesService) {
+    $scope.route = route;
+  
+    $scope.userLocation = LocationService.getCurrentLocation(function(data) {
+      $scope.map = MapService.createMap(data);
+
+      LocationService.displayUser($scope.map, data, './img/user.png', 3000);
+
+      RestBusService.getStationLocation($scope.map, route);
+
+      VehiclesService.displayVehicles($scope.map, $scope.route, './img/bus.png', 3000);
+
+    });
 
   });
--- www/js/services/services.js
+++ www/js/services/services.js
@@ -7,7 +7,7 @@ angular.module('app.services', [
   'ngCordova'
 ])
 
-.service('LocationService', function($cordovaGeolocation, $ionicPlatform, $ionicPopup) {
+.service('LocationService', function($cordovaGeolocation, $ionicPlatform, $ionicPopup, MapService) {
   /** 
    * Takes a callback whose first argument contains current location. Displays an error to the user if location cannot be found.
    * @param {func} callback - The function that recieves the lat and long
@@ -38,9 +38,30 @@ angular.module('app.services', [
         });
     });
   };
+
+  // display user on map
+  this.displayUser = function(map, loc, image, interval) {
+
+    var userMarker = MapService.createMarker(map, loc, image);
+
+    // update user position
+    if(interval !== undefined && interval > 0) {
+      if(interval < 1000) {
+        interval = 1000;
+      }
+
+      setInterval(function(){
+        this.getCurrentLocation(function(data) {
+          userMarker.setPosition(new google.maps.LatLng(data.latitude, data.longitude));
+        });
+      }.bind(this), interval);
+    }
+
+  };
+
 })
 
-.service('RestBusService', function($http, $q, LocationService, $ionicLoading) {
+.service('RestBusService', function($http, $q, $ionicLoading, LocationService, ReadFileService, MapService) {
   /** 
    * Gets the stations that are closest in proximity to the user 
    * @param {object} latlon - Object with a latitude and longitude
@@ -55,10 +76,11 @@ angular.module('app.services', [
       }).success(function(data) {
         routes = data;
         dfd.resolve(data);
-      })
+      });
     });
     return dfd.promise;
   };
+
   this.getRoute =  function(routeId) {
     var dfd  = $q.defer();
     routes.forEach(function(route) {
@@ -68,6 +90,87 @@ angular.module('app.services', [
     });
     return dfd.promise;
   };
+
+  this.getStationLocation = function(map, route) {
+
+    ReadFileService.readFile('../stops.json')
+    .then(function(data) {
+      var station = data.data[route.stop.id];
+      var loc = {latitude: station.lat, longitude: station.lon};
+      MapService.createMarker(map, loc);
+    });
+
+  };
+
+})
+
+.service('VehiclesService', function($http) {
+
+  // get list of vehicles with api
+  this.getVehicles = function() {
+    return $http({
+      url: 'http://mybus-api.herokuapp.com/agencies/sf-muni/vehicles',
+      method: 'GET'
+    });
+  };
+
+  // put vehicle on map
+  this.displayVehicle = function(map, loc, image) {
+    return new google.maps.Marker({
+        position: new google.maps.LatLng(loc.latitude, loc.longitude),
+        map: map,
+        icon: image
+      });
+  };
+
+  // create vehicles and display them on map
+  this.displayVehicles = function(map, route, image, interval) {
+    var displayVehicle = this.displayVehicle;
+    var vehicleMarkers = {};
+
+    //put vehicles on map
+    this.getVehicles()
+    .then(function(data) {
+      var vehicles = data.data;
+      var routeId = route.route.id;
+
+      for(var i = 0, len = vehicles.length; i < len; i++) {
+        if(vehicles[i].routeId === routeId) {
+          var loc = {latitude: vehicles[i].lat, longitude: vehicles[i].lon};
+          vehicleMarkers[vehicles[i].id] = displayVehicle(map, loc, image);
+        }
+      }
+
+    });
+
+    // refresh marker positions if given interval
+    if(interval !== undefined && interval > 0) {
+      if(interval < 1000) {
+        interval = 1000;
+      }
+
+      setInterval(function(){
+        this.getVehicles()
+        .then(function(data) {
+          var vehicles = data.data;
+          for(var i = 0, len = vehicles.length; i < len; i++) {
+            var id = vehicles[i].id;
+
+            if(vehicleMarkers[id]) {
+              var lat = vehicles[i].lat;
+              var lon = vehicles[i].lon;
+              vehicleMarkers[id].setPosition(new google.maps.LatLng(lat, lon));
+            }
+
+          }
+
+        });
+      }.bind(this), interval);
+
+    }
+
+  };
+
 })
 
 .service('ReadFileService', function($http) {
@@ -83,6 +186,26 @@ angular.module('app.services', [
     });
   };
   
+})
+
+.service('MapService', function() {
+
+  // create a map
+  this.createMap = function(loc) {
+    // var sanFran = {lat: 37.78, lng: -122.416}
+    var mapOptions = {center: {lat: loc.latitude, lng: loc.longitude}, zoom: 17};
+    return new google.maps.Map(document.getElementById('mapContainer'), mapOptions);
+  };
+
+  // create a map marker
+  this.createMarker = function(map, loc, image) {
+    return new google.maps.Marker({
+        position: new google.maps.LatLng(loc.latitude, loc.longitude),
+        map: map,
+        icon: image
+      });
+  };
+
 });
 
 
