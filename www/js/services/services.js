a0318aac9fa3f11163624a885a73375fa4ee8b29
Justin Fong <justinf715@gmail.com>
2015-06-29 12:07:03 -0700

    Fix duplicate routes close #89

--- www/js/services/services.js
+++ www/js/services/services.js
@@ -56,7 +56,7 @@ angular.module('app.services', [
    * Gets the stations that are closest in proximity to the user 
    * @param {object} latlon - Object with a latitude and longitude
    */
-  var routes = {};
+  var routes = [];
   this.getRoutes = function() {
     var dfd = $q.defer();
 
@@ -66,14 +66,6 @@ angular.module('app.services', [
         url: 'http://mybus-api.herokuapp.com/locations/' + latlon.latitude + ',' + latlon.longitude + '/predictions',
         method: 'GET'
       }).success(function(data) {
-        // console.log(routes);
-        for (var i = 0; i < data.length; i++) {
-          console.log(data[i]);
-          // if (!routes[data[i].route.title]) {
-          //   routes[data[i].route.title] = data[i];
-          // }
-        }
-        // console.log(routes);
         routes = data;
         dfd.resolve(data);
       });
